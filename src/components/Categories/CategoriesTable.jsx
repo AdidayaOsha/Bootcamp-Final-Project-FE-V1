fa707142924e52f93bbe56e46e52db794347219e
AdidayaOsha <adidayaosha@gmail.com>
2022-04-10 15:47:43 +0700

    added Add Category Feature

--- src/components/Categories/CategoriesTable.jsx
+++ src/components/Categories/CategoriesTable.jsx
@@ -1,120 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Axios from "axios";
+import { API_URL } from "../../constant/api";
 
 const CategoriesTable = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = async () => {
+    try {
+      await Axios.get(`${API_URL}/products/categories`).then((results) => {
+        setCategories(results.data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const TableHead = () => {
+    return (
+      <thead>
+        <tr>
+          <th>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" />
+            </div>
+          </th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th className="text-end">Action</th>
+        </tr>
+      </thead>
+    );
+  };
+
+  const TableBody = () => {
+    return categories.map((val, id) => {
+      return (
+        <tr key={val.id}>
+          <td>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" />
+            </div>
+          </td>
+          <td>{val.id}</td>
+          <td>
+            <b>{val.name}</b>
+          </td>
+          <td>{val.description}</td>
+          <td className="text-end">
+            <div className="dropdown">
+              <Link to="#" data-bs-toggle="dropdown" className="btn btn-light">
+                <i className="fas fa-ellipsis-h"></i>
+              </Link>
+              <div className="dropdown-menu">
+                <Link className="dropdown-item" to="#">
+                  Edit info
+                </Link>
+                <Link className="dropdown-item text-danger" to="#">
+                  Delete
+                </Link>
+              </div>
+            </div>
+          </td>
+        </tr>
+      );
+    });
+  };
+
   return (
     <div className="col-md-12 col-lg-8">
       <table className="table">
-        <thead>
-          <tr>
-            <th>
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" />
-              </div>
-            </th>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th className="text-end">Action</th>
-          </tr>
-        </thead>
-        {/* Table Data */}
+        <TableHead />
         <tbody>
-          <tr>
-            <td>
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" />
-              </div>
-            </td>
-            <td>1</td>
-            <td>
-              <b>Men clothes</b>
-            </td>
-            <td>Men clothes</td>
-            <td className="text-end">
-              <div className="dropdown">
-                <Link
-                  to="#"
-                  data-bs-toggle="dropdown"
-                  className="btn btn-light"
-                >
-                  <i className="fas fa-ellipsis-h"></i>
-                </Link>
-                <div className="dropdown-menu">
-                  <Link className="dropdown-item" to="#">
-                    Edit info
-                  </Link>
-                  <Link className="dropdown-item text-danger" to="#">
-                    Delete
-                  </Link>
-                </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" />
-              </div>
-            </td>
-            <td>2</td>
-            <td>
-              <b>Women fashion</b>
-            </td>
-            <td>Fashions for Women</td>
-
-            <td className="text-end">
-              <div className="dropdown">
-                <Link
-                  to="#"
-                  data-bs-toggle="dropdown"
-                  className="btn btn-light"
-                >
-                  <i className="fas fa-ellipsis-h"></i>
-                </Link>
-                <div className="dropdown-menu">
-                  <Link className="dropdown-item" to="#">
-                    Edit info
-                  </Link>
-                  <Link className="dropdown-item text-danger" to="#">
-                    Delete
-                  </Link>
-                </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" />
-              </div>
-            </td>
-            <td>3</td>
-            <td>
-              <b>Kids clothes</b>
-            </td>
-            <td>Clothes for kids</td>
-
-            <td className="text-end">
-              <div className="dropdown">
-                <Link
-                  to="#"
-                  data-bs-toggle="dropdown"
-                  className="btn btn-light"
-                >
-                  <i className="fas fa-ellipsis-h"></i>
-                </Link>
-                <div className="dropdown-menu">
-                  <Link className="dropdown-item" to="#">
-                    Edit info
-                  </Link>
-                  <Link className="dropdown-item text-danger" to="#">
-                    Delete
-                  </Link>
-                </div>
-              </div>
-            </td>
-          </tr>
+          <TableBody />
         </tbody>
       </table>
     </div>
