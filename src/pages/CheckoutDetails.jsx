55df792058a21134198952a2dea784863dde19b8
AdidayaOsha <adidayaosha@gmail.com>
2022-04-17 14:43:44 +0700

    Creating a Component for Each Cart List Details

--- src/pages/CheckoutDetails.jsx
+++ src/pages/CheckoutDetails.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import CartList from "../components/Checkout/CartList";
+import CartDetails from "../components/Checkout/CartDetails";
 import OrderProgress from "../components/Checkout/OrderProgress";
+import OrderSummary from "../components/Checkout/OrderSummary";
 
 const CheckoutDetails = () => {
   return (
     <>
-      <OrderProgress />
-      {/* <CartList /> */}
+      <div className="mt-20">
+        <OrderProgress />
+      </div>
+      <div className="flex w-screen space-x-4 pt-5 justify-end pr-48">
+        <CartDetails />
+        <OrderSummary />
+      </div>
     </>
   );
 };
