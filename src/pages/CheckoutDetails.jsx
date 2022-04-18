6dd9145f54d0ba23b6c758036b18665473fe9ccc
AdidayaOsha <adidayaosha@gmail.com>
2022-04-19 04:40:03 +0700

    improving Checkout and Billing Details

--- src/pages/CheckoutDetails.jsx
+++ src/pages/CheckoutDetails.jsx
@@ -6,9 +6,7 @@ import OrderSummary from "../components/Checkout/OrderSummary";
 const CheckoutDetails = () => {
   return (
     <>
-      <div className="mt-20">
         <OrderProgress />
-      </div>
       <div className="flex w-screen space-x-4 pt-5 justify-end pr-48">
         <CartDetails />
         <OrderSummary />
