08cb1a070ee1793c86baabbe13c4e7fd73d3bef4
AdidayaOsha <adidayaosha@gmail.com>
2022-04-25 23:34:15 +0700

    Added Nested Routes for Cart and Checkout

--- src/components/Checkout/OrderSummary.jsx
+++ src/components/Checkout/OrderSummary.jsx
@@ -3,33 +3,76 @@ import { useDispatch, useSelector } from "react-redux";
 import { currencyFormatter } from "../../helpers/currencyFormatter";
 
 const OrderSummary = () => {
-  const cartGlobal = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const [subTotal, setSubTotal] = useState(0);
   const [discount, setDiscount] = useState(0);
   const [shipping, setShipping] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+
+  console.log(
+    `sub: ${subTotal}, disc: ${discount}, Ship: ${shipping}, TotalPrice: ${totalPrice}`
+  );
+
+  const cartGlobal = useSelector((state) => state.cart);
+  const summaryGlobal = useSelector((state) => state.summary);
+  console.log(summaryGlobal);
 
   useEffect(() => {
-    const renderSubTotal = () => {
-      let total = 0;
-      cartGlobal.cartList.forEach((val) => {
-        total += val.subtotal;
-        setSubTotal(total);
-      });
+    const renderSubTotal = async () => {
+      try {
+        let total = 0;
+        await cartGlobal.cartList.forEach((val) => {
+          total += val.subtotal;
+          dispatch({
+            type: "GET_SUBTOTAL",
+            payload: total,
+          });
+          setSubTotal(total);
+        });
+      } catch (err) {
+        console.log(err);
+      }
     };
     renderSubTotal();
-  }, []);
+  }, [cartGlobal]);
 
   useEffect(() => {
     const renderTotalPrice = () => {
-      return subTotal + discount + shipping;
+      try {
+        let total = 0;
+        total = subTotal - discount + shipping;
+        dispatch({
+          type: "GET_TOTALPRICE",
+          payload: total,
+        });
+        setTotalPrice(total);
+      } catch (err) {
+        console.log(err);
+      }
     };
     renderTotalPrice();
-  }, [subTotal, discount, shipping]);
+  }, [cartGlobal, subTotal, discount, shipping]);
 
-  console.log("testis");
+  useEffect(() => {
+    const discountHandler = () => {
+      try {
+        let discountFee;
+        discountFee = totalPrice * 0.05;
+        setDiscount(discountFee);
+        setTotalPrice(totalPrice - discountFee);
+        dispatch({
+          type: "GET_DISCOUNT",
+          payload: discountFee,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    discountHandler();
+  }, [isClicked]);
+
   return (
     <>
       {/* RIGHT COL ORDER SUMMARY */}
@@ -44,33 +87,59 @@ const OrderSummary = () => {
               <h2 className="text-gray-400">Sub Total</h2>
               <h2 className="font-bold">{currencyFormatter(subTotal)}</h2>
             </div>
+            {/* DISCOUNT */}
             <div className="flex justify-between">
               <h2 className="text-gray-400">Discount</h2>
-              <h2 className="font-bold">-</h2>
+              {summaryGlobal.discount ? (
+                <h2 className="">
+                  {currencyFormatter(summaryGlobal.discount)}
+                </h2>
+              ) : (
+                <h2 className="">-</h2>
+              )}
             </div>
             <div className="flex justify-between">
               <h2 className="text-gray-400">Shipping</h2>
               <h2 className="font-bold">Free</h2>
             </div>
             <span className="flex border-top h-[2px] bg-slate-100 w-full"></span>
+            {/* TOTAL PRICE */}
             <div className="flex justify-between">
               <div className="text-lg font-bold">
                 <h2>Total</h2>
               </div>
               <div className="">
                 <h2 className="text-lg font-bold text-red-400 text-right">
-                  Rp. {}
+                  {currencyFormatter(totalPrice)}
                 </h2>
                 <p className="text-xs font-extralight text-right italic">
                   (PPN Included if Applicable)
                 </p>
               </div>
             </div>
+            {summaryGlobal.discount ? (
+              <div className="w-7/12 m-auto mt-3 rounded-lg bg-orange-50 ">
+                <h2 className="text-yellow-500 text-center italic">
+                  Discount Has Been Applied!
+                </h2>
+              </div>
+            ) : null}
             <div className="border-2 rounded-md flex justify-between p-2">
               <h2 className="text-lg">DISCOUNT5</h2>
-              <button className="btn btn-ghost text-accent btn-sm">
-                Apply
-              </button>
+              {summaryGlobal.discount ? (
+                <button className="btn btn-ghost text-gray-600 bg-gray-200 btn-sm disabled">
+                  Apply
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    setIsClicked(true);
+                  }}
+                  className="btn btn-ghost text-accent btn-sm"
+                >
+                  Apply
+                </button>
+              )}
             </div>
           </div>
         </div>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import adminReducer from "./admin";
 import userReducer from "./user";
 import cartReducer from "./cart";
+import summaryReducer from "./summary";
 
 export default combineReducers({
   admin: adminReducer,
   user: userReducer,
   cart: cartReducer,
+  summary: summaryReducer,
 });
