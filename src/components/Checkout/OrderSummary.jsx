982d05bde39f10197959870c53e26cbba1f03e6d
AdidayaOsha <adidayaosha@gmail.com>
2022-04-25 11:43:16 +0700

    Version Summary

--- src/components/Checkout/OrderSummary.jsx
+++ src/components/Checkout/OrderSummary.jsx
@@ -29,7 +29,7 @@ const OrderSummary = () => {
     renderTotalPrice();
   }, [subTotal, discount, shipping]);
 
-  console.log("testi");
+  console.log("testis");
   return (
     <>
       {/* RIGHT COL ORDER SUMMARY */}
